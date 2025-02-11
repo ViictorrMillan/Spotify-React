54780aa3d40a4c0ae5606242d000f577f363d848
Victor Millan <victorhugomartinsmillan@hotmail.com>
2025-02-11 18:17:31 -0300

    Feat: Header Adicionado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// App.jsx
+import React, { useState } from 'react';
+import './styles/global.css';	
 import './App.css';
+import Header from '../src/components/Header/Header';
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header onSearch={handleSearch} />
     </div>
   );
 }
